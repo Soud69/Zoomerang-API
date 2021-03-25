7706eee19086a91a0c8f8fde1f8e80593420d353
Soud <57543084+Soud69@users.noreply.github.com>
2021-03-25 13:03:52 +0300

    Create InstaBio-API.py

---
+++ API/InstaBio-API.py
@@ -0,0 +1,39 @@
+import requests
+# Importing requests
+
+# Note: We Need Auth Token Fron Login-API.py
+# token = json.loads(req.text)["idToken"]
+
+url = "https://us-central1-zoomerang-dcf49.cloudfunctions.net/apiv2/v2/user/update"
+# Instagram API URL
+
+username = input("Intagram Username: ")
+# Username Input
+
+data = {"instagram": username}
+# Instagram API Data
+
+headers = {
+	"Host": "us-central1-zoomerang-dcf49.cloudfunctions.net",
+	"Content-Type": "application/json",
+	"Connection": "keep-alive",
+	"Accept": "application/json",
+	"User-Agent": "Zoomerang/3.0.16 (com.yantech.zoomerang; build:3; iOS 14.2.1) Alamofire/5.2.2",
+	"Authorization": f"Bearer {token}",
+	"Accept-Language": "en-US;q=1.0, ar-US;q=0.9",
+	"Accept-Encoding": "br;q=1.0, gzip;q=0.9, deflate;q=0.8"
+	}
+# Instagram API Headers
+
+req = requests.put(url, json=data, headers=headers)
+# Instagram API Requests
+
+if f'instagram":"{username}"' in req.text:
+	print("Changed User Success")
+	# Instagram Success Changed
+
+else:
+	print("Error !")
+	print(req)
+	print(req.text)
+	# Error or Something Wrong
